5fbc18b3839fa8d513c44c9191ecfa7c54cf172a
Oscar Nuki <32140106+nuki111@users.noreply.github.com>
2020-04-28 23:12:17 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='env_explore',
+      version='0.0.1',
+      description='env_explore: Quick and easy exploration of python objects.',
+      author='Oscar Nuki',
+      author_email='oscar.nuki@gmail.com',
+      packages=find_packages(),
+    )
